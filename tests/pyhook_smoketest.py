96bcacc4b1e0bf828cf7a3642096b6298d9e6126
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-01-01 18:49:48 +0200

    test

--- tests/pyhook_smoketest.py
+++ tests/pyhook_smoketest.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
-import sys
-import os
-print("SMOKE ENV", os.environ, flush=True)
-print("SMOKE ORIG_ARG", sys.orig_argv, flush=True)
-print("SMOKE VER", sys.version, flush=True)
-print("SMOKE PATH", sys.path, flush=True)
-
 import fontforge
 import psMat
 import pickle
