30f097270e15c1c8651822b17d4056ac5a802a00
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-01-03 10:11:22 +0200

    Localization class L10nText

--- fontforgeexe/gtk/menu_builder.hpp
+++ fontforgeexe/gtk/menu_builder.hpp
@@ -30,6 +30,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
+#include <libintl.h>
 #include <gtkmm-3.0/gtkmm.h>
 
 namespace FF {
@@ -44,8 +45,25 @@ enum CheckableState : bool {
     NonCheckable = false
 };
 
+// Seamlessly localize a string using implicit constructor and conversion.
+class L10nText {
+   public:
+    L10nText(const char* text) : text_(text) {}
+
+    operator Glib::ustring() const {
+      if (!text_.empty() && l10n_text_.empty()) {
+         l10n_text_ = gettext(text_.c_str());
+      }
+      return l10n_text_;
+    }
+
+   private:
+    Glib::ustring text_;
+    mutable Glib::ustring l10n_text_;
+};
+
 struct LabelInfo {
-    Glib::ustring text;
+    L10nText text;
     std::string image_file;
     EnabledState enabled;
     CheckableState checkable;
