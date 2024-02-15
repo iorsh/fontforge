8f247cc44658f8cf2ce163ba7fa16f21dc8d4d26
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-02-16 00:04:19 +0200

    Disable legacy tooltips in Font View

--- fontforgeexe/fontview.c
+++ fontforgeexe/fontview.c
@@ -6621,18 +6621,15 @@ static void FVMouse(FontView *fv, GEvent *event) {
     int gid;
     int realpos = pos;
     SplineChar *sc, dummy;
-    int dopopup = true;
 
     if ( event->type==et_mousedown )
 	CVPaletteDeactivate();
     if ( pos<0 ) {
 	pos = 0;
-	dopopup = false;
     } else if ( pos>=fv->b.map->enccount ) {
 	pos = fv->b.map->enccount-1;
 	if ( pos<0 )		/* No glyph slots in font */
 return;
-	dopopup = false;
     }
 
     sc = (gid=fv->b.map->map[pos])!=-1 ? fv->b.sf->glyphs[gid] : NULL;
@@ -6684,9 +6681,6 @@ return;
 	    } else
 		BitmapViewCreate(bc,bdf,fv,pos);
 	}
-    } else if ( event->type == et_mousemove ) {
-	if ( dopopup )
-	    SCPreparePopup(fv->v,sc,fv->b.map->remap,pos,sc==&dummy?dummy.unicodeenc: UniFromEnc(pos,fv->b.map->enc));
     }
     if ( event->type == et_mousedown ) {
 	if ( fv->drag_and_drop ) {
@@ -6761,8 +6755,6 @@ return;
 	    fv->pressed = NULL;
 	}
     }
-    if ( event->type==et_mouseup && dopopup )
-	SCPreparePopup(fv->v,sc,fv->b.map->remap,pos,sc==&dummy?dummy.unicodeenc: UniFromEnc(pos,fv->b.map->enc));
     if ( event->type==et_mouseup )
 	SVAttachFV(fv,2);
 }
