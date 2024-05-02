de94bcf168777ba81aace41f88b2ac991e7455c3
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-05-02 23:52:15 +0300

    GTK utils gtk_get_keyboard_state(), set/unset_cursor()

--- fontforgeexe/gtk/utils.hpp
+++ fontforgeexe/gtk/utils.hpp
@@ -58,3 +58,10 @@ Gtk::Widget* gtk_find_child(Gtk::Widget* w, const std::string& name);
 int label_offset(Gtk::Widget* w);
 
 Glib::RefPtr<Gdk::Pixbuf> load_icon(const Glib::ustring& icon_name, int size);
+
+Gdk::ModifierType gtk_get_keyboard_state();
+
+Glib::RefPtr<Gdk::Cursor> set_cursor(Gtk::Widget* widget,
+                                     const Glib::ustring& name);
+
+void unset_cursor(Gtk::Widget* widget, Glib::RefPtr<Gdk::Cursor> old_cursor);
