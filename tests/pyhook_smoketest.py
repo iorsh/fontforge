0ec464b091952809c8fabb7280e622233141c02e
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-05-22 20:14:34 +0300

    smoketests messages

--- tests/pyhook_smoketest.py
+++ tests/pyhook_smoketest.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+import sys
+import sysconfig as sc
+import distutils.sysconfig as d_sc
+
+print("pysys paths", sys.path)
+print("sysconfig paths", sc.get_paths())
+print("distutils sysconfig paths", d_sc.get_python_lib(prefix='', plat_specific=True,standard_lib=False))
+
 from __future__ import print_function
 import fontforge
 import psMat
