df360fadc7498274922879c5b35ac0c27cf206de
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-05-24 00:31:15 +0300

    smoke

--- tests/pyhook_smoketest.py
+++ tests/pyhook_smoketest.py
@@ -26,9 +26,6 @@ import fontforge
 import psMat
 import pickle
 
-print("fontforge module path", os.path.abspath(fontforge.__file__))
-print("psMat module path", os.path.abspath(psMat.__file__))
-
 print(fontforge.__version__, fontforge.version())
 
 fontforge.font()
