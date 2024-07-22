d025bec2dfde8754a4e201c4e19d7d8738ce5af0
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-07-22 22:44:20 +0300

    Revert "Cache menu items"
    This reverts commit 76e624e3a79364602a8ba47e9aee6c755e9e5daa.

--- fontforgeexe/gtk/ui_context.hpp
+++ fontforgeexe/gtk/ui_context.hpp
@@ -30,7 +30,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
-#include <unordered_map>
 #include <gtkmm/accelgroup.h>
 #include <gtkmm.h>
 
@@ -53,8 +52,6 @@ public:
     virtual Glib::RefPtr<Gtk::AccelGroup> get_accel_group() const = 0;
 
     Gtk::Window* window_ = nullptr;
-
-    mutable std::unordered_map<std::string, Gtk::MenuItem*> menu_cache;
 };
 
 }
