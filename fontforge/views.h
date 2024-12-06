ee4f3a3893129a79a7fae99fb1de317f04324007
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-12-06 10:33:37 +0200

    Remove legacy infobar

--- fontforge/views.h
+++ fontforge/views.h
@@ -438,7 +438,7 @@ typedef struct fontview {
     GIC *gic;
     GIC *gwgic;
     int width, height;		/* of v */
-    int16_t infoh,mbh;
+    int16_t mbh;
     int16_t lab_height, lab_as;
     int16_t colcnt, rowcnt;		/* of display window */
     int32_t rowoff, rowltot;		/* Can be really big in full unicode */
@@ -689,7 +689,7 @@ struct kf_dlg /* : fvcontainer */ {
     GFont *plain, *bold;
     int fh, as;
     GGadget *mb, *guts, *topbox;
-    int mbh, label2_y, infoh;
+    int mbh, label2_y;
 
     SplineFont *sf;
     int def_layer;
