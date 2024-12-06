e13beea2f914251397afe943b226fad6d9486b14
Maxim Iorsh <iorsh@users.sourceforge.net>
2024-12-06 10:33:37 +0200

    Localization class L10nText

--- fontforgeexe/gtk/utils.hpp
+++ fontforgeexe/gtk/utils.hpp
@@ -27,7 +27,25 @@
 #pragma once
 
 #include <string>
+#include <libintl.h>
 #include <gtkmm.h>
+
+// Seamlessly localize a string using implicit constructor and conversion.
+class L10nText {
+ public:
+    L10nText(const char* text) : text_(text) {}
+
+    operator Glib::ustring() const {
+        if (!text_.empty() && l10n_text_.empty()) {
+            l10n_text_ = gettext(text_.c_str());
+        }
+        return l10n_text_;
+    }
+
+ private:
+    Glib::ustring text_;
+    mutable Glib::ustring l10n_text_;
+};
 
 double ui_font_em_size();
 double ui_font_eX_size();
