2cf733ffed2a247be1f9b8e5ab2a09ecff60e0f8
Maxim Iorsh <iorsh@users.sourceforge.net>
2025-09-22 12:42:18 +0300

    [temp] hdiutil -verbose

--- .github/workflows/scripts/ffosxbuild.sh
+++ .github/workflows/scripts/ffosxbuild.sh
@@ -72,7 +72,7 @@ if [ ! -z "$CI" ]; then
         dmgname=FontForge-$builddate-${HASH:0:7}.app.dmg
     fi
 
-    hdiutil create -size 800m   \
+    hdiutil create -size 800m -verbose  \
         -fs HFS+                 \
         -volname   FontForge     \
         -srcfolder $APPDIR       \
