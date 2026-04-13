e8e8e2c80d2360c8eb8d7a61aaad3fd7bd68e02d
Maxim Iorsh <iorsh@users.sourceforge.net>
2026-04-13 22:07:27 +0300

    class LegacyPrinter

---
+++ fontforge/layout/legacy_printer.hpp
@@ -0,0 +1,71 @@
+/* Copyright 2026 Maxim Iorsh <iorsh@users.sourceforge.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+
+ * The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#pragma once
+
+#include <utility>
+
+#include "i_printer.hpp"
+
+struct printinfo;
+
+namespace ff::layout {
+
+enum class PrintPageStyle {
+    regular,
+    sample,
+};
+
+struct PrinterContext {
+    ::printinfo* pi;
+    void (*start_document_cb)(::printinfo*);
+    void (*end_document_cb)(::printinfo*);
+    void (*add_regular_page_cb)(::printinfo*);
+    void (*add_sample_page_cb)(::printinfo*);
+};
+
+class LegacyPrinter final : public IPrinter {
+ public:
+    LegacyPrinter(PrintPageStyle page_style, PrinterContext context)
+        : page_style_(page_style), context_(std::move(context)) {}
+
+    void start_document() override { context_.start_document_cb(context_.pi); }
+
+    void end_document() override { context_.end_document_cb(context_.pi); }
+
+    void add_page() override {
+        if (page_style_ == PrintPageStyle::sample)
+            context_.add_sample_page_cb(context_.pi);
+        else
+            context_.add_regular_page_cb(context_.pi);
+    }
+
+ private:
+    PrintPageStyle page_style_;
+    PrinterContext context_;
+};
+
+}  // namespace ff::layout
