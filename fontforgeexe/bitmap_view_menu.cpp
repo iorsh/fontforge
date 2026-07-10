140523a4902ca1bca4a79b8eb212404da23e5a86
Maxim Iorsh <iorsh@users.sourceforge.net>
2026-07-10 17:13:26 +0300

    Use SVG icons in Bitmap View toolbar

--- fontforgeexe/bitmap_view_menu.cpp
+++ fontforgeexe/bitmap_view_menu.cpp
@@ -39,9 +39,9 @@ std::vector<BitmapViewTool> bitmap_view_tools = {
     {bvt_line, "line", "", N_("Draw a Line")},
     {bvt_shift, "shift", "all-scroll", N_("Shift Entire Bitmap")},
     {bvt_hand, "hand", "grab", N_("Scroll Bitmap")},
-    {bvt_rect, "toolsrect", "", N_("Draw Rectangle")},
-    {bvt_filledrect, "filledrect", "", N_("Draw Filled Rectangle")},
-    {bvt_elipse, "toolselipse", "", N_("Draw Ellipse")},
-    {bvt_filledelipse, "filledelipse", "", N_("Draw Filled Ellipse")},
+    {bvt_rect, "rectangle", "", N_("Draw Rectangle")},
+    {bvt_filledrect, "rectangle_fill", "", N_("Draw Filled Rectangle")},
+    {bvt_elipse, "ellipse", "", N_("Draw Ellipse")},
+    {bvt_filledelipse, "ellipse_fill", "", N_("Draw Filled Ellipse")},
 };
 void* p_bitmap_view_tools = &bitmap_view_tools;
